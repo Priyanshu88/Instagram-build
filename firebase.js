5b68c21798ec404a917228020423af4484c12490
PRIYANSHU PANDEY <86107841+Priyanshu88@users.noreply.github.com>
2022-09-26 15:09:11 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,13 +10,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBRJddSqU69v99JK3YCSYrvckYaYrCw5VI",
-    authDomain: "insta-build-109d2.firebaseapp.com",
-    projectId: "insta-build-109d2",
-    storageBucket: "insta-build-109d2.appspot.com",
-    messagingSenderId: "927920016487",
-    appId: "1:927920016487:web:e47b45052c8de9a84118c2",
-    measurementId: "G-28PVDKCY5X"
+    // config
 };
 
 // Initialize Firebase
